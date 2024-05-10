527ee7ebbed204df5c7bfc8aca704e909d6e27cb
khushal024 <khushalranka@gmail.com>
2024-05-10 13:24:38 +0530

    url fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       
-      <BrowserRouter basename={'/frontend'} >  
+      <BrowserRouter basename={'./'} >  
       <Header />
 
         <Routes>
