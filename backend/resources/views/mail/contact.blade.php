73fb7a044fbaf567ee51794648711a10b23e931c
khushal024 <khushalranka@gmail.com>
2024-06-18 19:04:52 +0530

    contact fields added

--- backend/resources/views/mail/contact.blade.php
+++ backend/resources/views/mail/contact.blade.php
@@ -1,6 +1,6 @@
 Name : {{ $request['name'] }}<br>
 Email : {{ $request['email'] }}<br>
-Phone : {{ $request['phone'] }}<br>
+Phone : {{ $request['contact_no'] }}<br>
 Message : {{ $request['message'] }}<br>
 Designation : {{ $request['designation'] }}<br>
 Company : {{ $request['company'] }}
